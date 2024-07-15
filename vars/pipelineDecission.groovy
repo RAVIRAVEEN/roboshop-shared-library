16f6b0d782b86529e8ee2846818a678c3f980e13
localstar1996 <localstar1996@gmail.com>
2024-07-15 22:12:06 +0530

    jenkins

--- vars/pipelineDecission.groovy
+++ vars/pipelineDecission.groovy
@@ -1,25 +1,5 @@
 #! groovy
 
-// def   decidepipeline (Map configMap) {
-//     application = configMap.get{"application"}
-//     switch(application) {
-//         case 'nodejsVM':
-//         nodejsVM (configMap)  
-//         break
-//         case 'javaVM':
-//         javaVM (configMap)  
-//         break
-//         case 'nodejsEKS':
-//         nodejsEKS(configMap)  
-//         break
-
-//         default:
-//             error = "application is not recognised"
-//         break
-//     }
-
-// }
-
 
 def decidePipeline(Map configMap){
     application = configMap.get("application")
