67efdc7b8ea04994b7e6ffe2c228a7f1a88bc1e7
localstar1996 <localstar1996@gmail.com>
2024-07-16 10:06:32 +0530

    jenkins

--- vars/pipelineDecission.groovy
+++ vars/pipelineDecission.groovy
@@ -1,15 +1,15 @@
 #! groovy
 def decidePipeline(Map configMap){
-    application=configMap.get("application")
-    switch(application){
-        case'nodejsVM':
-        nodejsVM(configMap)
+    application = configMap.get("application")
+    switch (application) {
+        case 'nodejsVM':
+            nodejsVM(configMap)
             break
-        case'javaVM':
-        javaVM(configMap)
+        case 'javaVM':
+            javaVM(configMap)
             break
         case'nodejsEKS':
-        nodejsEKS(configMap)
+            nodejsEKS(configMap)
             break
         default:
         error "Application is not recognised"
